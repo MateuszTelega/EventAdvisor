55da0ab643849cfc69b181a7f1f562053adb66df
Tomek <Polanglo1993>
2020-10-04 12:53:24 +0200

    add comment model 1.1

--- core/models.py
+++ core/models.py
@@ -7,10 +7,10 @@ class Event(models.Model):
 
 
 class Comment(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    description = models.TextField(max_length=200, null=False, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(max_length=500, null=False, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} comment {self.event} at {self.date}"
+        return f"{self.user} comment {self.event} at {self.created}"
