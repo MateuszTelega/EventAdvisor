7dfc238a17f0842ab5609dd87664a0be948da538
Tomek <Polanglo1993>
2020-10-10 10:47:46 +0200

    add search event function

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import EventCreateView
+from .views import EventCreateView, SearchEventView
 
 app_name = 'core'
 urlpatterns = [
     path('event/create', EventCreateView.as_view(), name='event_create'),
+    path('event/search', SearchEventView.as_view(), name='event_search'),
 ]
