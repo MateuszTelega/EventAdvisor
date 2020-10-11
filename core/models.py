a5acb275c4fbada67f8f43e51ce750ac8cd7efad
Amadeusz Walas <amadeusz.walas@gmail.com>
2020-10-11 09:27:52 +0200

    modified urls for picture display

--- core/models.py
+++ core/models.py
@@ -17,7 +17,7 @@ class Event(models.Model):
     place = models.CharField(max_length=100)
     start_time = models.TimeField()
     end_time = models.TimeField(null=True, blank=True)
-    picture = models.ImageField(upload_to="static/image", null=True)
+    picture = models.ImageField(upload_to="media", null=True)
     description = models.TextField(max_length=1000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     event_type = models.ForeignKey(EventType, null=True, blank=True, on_delete=models.SET_NULL)
